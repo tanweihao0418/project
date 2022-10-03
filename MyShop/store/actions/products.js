5776ccb4111c6e1bd66cd411f92027d00718d07e
tanweihao0418 <tanweihao0418@e.newera.edu.my>
2022-10-03 17:35:36 +0800

    edit

--- MyShop/store/actions/products.js
+++ MyShop/store/actions/products.js
@@ -42,7 +42,18 @@ export const fetchProducts = () => {
 }
 
 export const deleteProduct = productId => {
-  return { type: DELETE_PRODUCT, pid: productId };
+  return async dispatch=>{
+    const response = await fetch(
+      `https://react-32633-default-rtdb.firebaseio.com/products/${productId}.json`,
+      {
+        method:'DELETE',
+    });
+    if (!response.ok) {
+      throw new Error('Something went wrong!')
+    }
+    dispatch({ type: DELETE_PRODUCT, pid: productId });
+
+  }
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
@@ -79,13 +90,35 @@ export const createProduct = (title, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-  return {
-    type: UPDATE_PRODUCT,
-    pid: id,
-    productData: {
-      title,
-      description,
-      imageUrl,
+  return async dispatch =>{
+    const response=await fetch(
+      `https://react-32633-default-rtdb.firebaseio.com/products/${id}.json`,
+      {
+      method:'PATCH',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({
+        title,
+        description,
+        imageUrl
+      })
+    });
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!')
     }
+
+    dispatch( {
+      type: UPDATE_PRODUCT,
+      pid: id,
+      productData: {
+        title,
+        description,
+        imageUrl,
+      }
+    });
   };
+
+  
 };
